417ba7f30959045973e93f3cb9ca2e54b2d84a51
Eric Turner <eric.turner@ditto.live>
2023-06-06 14:55:35 -0700

    Initial Commit

---
+++ DittoBase/DittoBaseApp.swift
@@ -0,0 +1,18 @@
+///
+//  DittoBaseApp.swift
+//  DittoBase
+//
+//  Created by Eric Turner on 6/6/23.
+//
+//  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+
+import SwiftUI
+
+@main
+struct DittoBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
