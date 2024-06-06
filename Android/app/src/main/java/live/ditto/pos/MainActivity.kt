cbcaaf11746f7c855171bc48ecb2f52c2833e6ea
Rajesh Ramsaroop <rajesh.ramsaroop@ditto.live>
2024-06-06 18:08:32 -0400

    check ditto permissions on launch

--- Android/app/src/main/java/live/ditto/pos/MainActivity.kt
+++ Android/app/src/main/java/live/ditto/pos/MainActivity.kt
@@ -3,16 +3,36 @@ package live.ditto.pos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import live.ditto.pos.core.presentation.composables.screens.PosKdsApp
+import live.ditto.pos.core.presentation.viewmodel.PosKdsViewModel
+import live.ditto.transports.DittoSyncPermissions
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: PosKdsViewModel by viewModels<PosKdsViewModel>()
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+        viewModel.requireDitto().refreshPermissions()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        checkDittoPermissions()
+
         setContent {
             PosKdsApp()
+        }
+    }
+
+    private fun checkDittoPermissions() {
+        val missing = DittoSyncPermissions(this).missingPermissions()
+        if (missing.isNotEmpty()) {
+            requestPermissionLauncher.launch(missing)
         }
     }
 }
