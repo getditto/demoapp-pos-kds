f373dcfea2ce06d1d3b30db03990f9a436a4dcaf
Rajesh Ramsaroop <rajesh.ramsaroop@ditto.live>
2024-11-01 12:47:01 -0400

    update version code and number for android

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "live.ditto.pos"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
