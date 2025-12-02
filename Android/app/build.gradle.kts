dac370c288527042aef1c98ad6c063ed2d7cb543
Shunsuke Kondo <shunsuke@ditto.com>
2025-12-02 15:26:04 +0900

    versionName to 1.0.0

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 7
-        versionName = "1"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
